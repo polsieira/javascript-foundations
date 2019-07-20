5fcbbfb2783fc6888ebd9ba5ab5b6b5330f28c50
posi7790 <polasp17@gmail.com>
2019-07-19 20:06:52 -0600

    Complete ogre test

--- mythical-creatures/exercises/ogre.js
+++ mythical-creatures/exercises/ogre.js
@@ -7,10 +7,18 @@ class Ogre {
 
   encounter(human) {
     human.encounterCounter ++;
+    if (human.encounterCounter % 3 === 0) {
+      this.swingAt(human);
+    }
   }
 
   swingAt(human) {
-    this.swings ++;
+    this.swings++;
+    human.knockedOut = this.swings % 2 === 0 ? true : false;
+  }
+
+  apologize(human) {
+    human.knockedOut = false;
   }
 }
 
